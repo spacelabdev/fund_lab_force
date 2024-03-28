88b499e5724ee6128d99f37cea3ff91277a2a277
Melanie Winter <melaniebucher830@gmail.com>
2024-03-28 15:13:00 -0700

    Adds MobileNav bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,27 @@
-import Navbar from './components/Navbar/Navbar'
-import Homepage from './pages/Homepage/Homepage'
-import './App.css'
+import React, { useState, useEffect } from 'react';
+import Navbar from './components/Navbar/Navbar';
+import Homepage from './pages/Homepage/Homepage';
+import './App.css';
+import MobileNav from './components/MobileNav/MobileNav';
 
 function App() {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768)
+    }
+    handleResize();
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, [])
+
   return (
     <main className='App'>
-      <Navbar />
+      {isMobile ? <MobileNav /> : <Navbar />}
       <Homepage />
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/MobileNav/MobileNav.jsx
@@ -0,0 +1,16 @@
+import Logo from "../../../public/assets/spacelab.svg"
+import './MobileNav.css'
+
+export default function MobileNav() {
+    return (
+        <nav className="MobileNav">
+            <ul>
+                <li>
+                    <img src={Logo} alt="logo" className="logo"/>
+                </li>
+                <li>About</li>
+                <li>Donate</li>
+            </ul>
+        </nav>
+    )
+}
