c132453452e84180d95b27db8ebacf9c94d0c746
Melanie Winter <melaniebucher830@gmail.com>
2024-03-28 12:21:54 -0700

    Adds hero and navbar

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Navbar from './components/Navbar/Navbar'
+import Homepage from './pages/Homepage/Homepage'
+import './App.css'
+
+function App() {
+  return (
+    <main className='App'>
+      <Navbar />
+      <Homepage />
+    </main>
+  )
+}
+
+export default App
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import './Navbar.css'
+
+export default function Navbar() {
+    return (
+        <nav className="Navbar">
+            <ul>
+                <li>SpaceLab</li>
+                <li>Home</li>
+                <li>About</li>
+                <li>Donate</li>
+            </ul>
+        </nav>
+    )
+}
