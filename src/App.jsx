af57c21233dff5422d1218327e1e73803762dc53
aanand93 <alex329@mac.com>
2024-04-26 17:46:00 -0400

    add mission page layout page and component

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
-import { useState, useEffect } from 'react';
+// import { useState, useEffect } from 'react';
 import Navbar from './components/Navbar/Navbar';
-import Homepage from './pages/Homepage/Homepage';
+// import Homepage from './pages/Homepage/Homepage';
+import MissionPage from './pages/MissionPage/MissionPage';
 import Footer from './components/Footer/Footer';
 import './App.css';
 
 function App() {
-  return (
-    <main className='App'>
-      <Navbar />
-      <Homepage />
-      <Footer />
-    </main>
-  )
+	return (
+		<main className='App'>
+			<Navbar />
+			{/* <Homepage /> */}
+			<MissionPage />
+			<Footer />
+		</main>
+	);
 }
 
-export default App
+export default App;
 
 // Make sections default 100vh
---
+++ src/pages/MissionPage/MissionPage.jsx
@@ -0,0 +1,12 @@
+import Mission from '../../components/Mission/Mission';
+
+import './MissionPage.css';
+
+export default function MissionPage() {
+	return (
+		<main className='MissionPage'>
+			<Mission />
+			<div></div>
+		</main>
+	);
+}
