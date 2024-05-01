7c44e980a2cf7b755790c04fbf65b83af15031d4
Melanie Winter <melaniebucher830@gmail.com>
2024-05-01 00:54:18 -0700

    Adds tagline to hero. Still needs work on spacing text on large screen sizes

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -4,21 +4,9 @@ export default function Hero({ playAnimation }) {
     return (
         <section className={`Hero ${playAnimation ? 'hero-animate' : ''}`}>
             <div className='hero-content'>
-                <h1>FundLab Force</h1>
-                <div className='hero-p-div'>
-                    <p>
-                        As diversity fuels creativity and innovation, we here at SpaceLab strongly value the voices and perspectives of those underrepresented in the community.
-                    </p>
-                    <p>
-                        One of our goals is to create paid fellowships, internships, scholarship opportunities, and debt relief for those who are underrepresented, including but not limited to women, members of the LGBTQ+ community, and people of color. Helping members of the community who struggle economically will provide them with the liberty and flexibility necessary to explore new career paths in science and technology.
-                    </p>
-                    <p>
-                        To help us grow these opportunities, please consider making a tax-deductible donation to SpaceLab today.
-                    </p>
-                </div>
-                    <p className='construction-note'>
-                        🚧 Website is under construction... please stay tuned for more! 🚧
-                    </p>
+                <p>
+                    Unlock the Universe: Support <span className='span-2'>Spacelab’s Quest</span>  for Inclusivity and Innovation
+                </p>
             </div>
         </section>
     )
