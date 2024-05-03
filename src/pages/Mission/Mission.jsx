eee5f950ef7914632a0a8997866701bbcb658386
Melanie Winter <melaniebucher830@gmail.com>
2024-05-03 04:05:43 -0700

    Adds more functionality to new parallax feature

--- src/pages/Mission/Mission.jsx
+++ src/pages/Mission/Mission.jsx
@@ -13,17 +13,38 @@ export default function Mission () {
                     img="./assets/Images/parallax-1.jpg"
                     subheading="Check out what we have to say..."
                     heading="Values"
-                ></ParallaxContent>
+                >
+                    <div className='temp'>
+                        <h2>This is cool!</h2>
+                        <p>
+                            At the heart of our organization lies a deep appreciation of the awesome power of diversity in problem-solving. We firmly believe that when individuals from varied backgrounds come together, their collective wisdom and perspectives lead to innovative and impactful solutions that address the unique needs of all. Grounded in principles of equity, inclusion, and belonging, we strive to create an environment where every voice is not only heard but celebrated. Join us in our journey to champion diversity, equity, and inclusion in data science, technology, and STEM fields like astronomy, as we work tirelessly to create a brighter and more inclusive future.
+                        </p> {/* Turn this into its own styled component */}
+                    </div>
+                </ParallaxContent>
                 <ParallaxContent
                     img="./assets/Images/parallax-2.jpg"
                     subheading="Check out what we have to say..."
                     heading="Mission"
-                ></ParallaxContent>
+                >
+                    <div className='temp'>
+                        <h2>This is cool!</h2>
+                        <p>
+                            At the heart of our organization lies a deep appreciation of the awesome power of diversity in problem-solving. We firmly believe that when individuals from varied backgrounds come together, their collective wisdom and perspectives lead to innovative and impactful solutions that address the unique needs of all. Grounded in principles of equity, inclusion, and belonging, we strive to create an environment where every voice is not only heard but celebrated. Join us in our journey to champion diversity, equity, and inclusion in data science, technology, and STEM fields like astronomy, as we work tirelessly to create a brighter and more inclusive future.
+                        </p> {/* Turn this into its own styled component */}
+                    </div>
+                </ParallaxContent>
                 <ParallaxContent
                     img="./assets/Images/parallax-3.jpg"
                     subheading="Check out what we have to say..."
                     heading="Vision"
-                ></ParallaxContent>
+                >
+                    <div className='temp'>
+                        <h2>This is cool!</h2>
+                        <p>
+                            At the heart of our organization lies a deep appreciation of the awesome power of diversity in problem-solving. We firmly believe that when individuals from varied backgrounds come together, their collective wisdom and perspectives lead to innovative and impactful solutions that address the unique needs of all. Grounded in principles of equity, inclusion, and belonging, we strive to create an environment where every voice is not only heard but celebrated. Join us in our journey to champion diversity, equity, and inclusion in data science, technology, and STEM fields like astronomy, as we work tirelessly to create a brighter and more inclusive future.
+                        </p> {/* Turn this into its own styled component */}
+                    </div>
+                </ParallaxContent>
                 {/* Turn this stuff into a .json, add content prop as well, instead of children */}
                 {/* Find videos for this instead of photos? */}
             </div>
