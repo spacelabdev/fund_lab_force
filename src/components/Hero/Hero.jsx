475fd3d76acdd520abcac63de6244015142f4ee0
Melanie Winter <melaniebucher830@gmail.com>
2024-05-10 23:16:47 -0700

    Adds random sizing function to photo integration

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,7 +1,6 @@
 import './Hero.css';
 
 export default function Hero({ playAnimation }) {
-
     function getAllPhotos() {
         const basePath = './assets/Images/';
         const numPhotos = 4;
@@ -24,22 +23,35 @@ export default function Hero({ playAnimation }) {
 
     const shuffledPhotos = allPhotos.sort(() => Math.random() - 0.5)
 
+    const contentWidth = 30;
+
     return (
         <section className={`Hero ${playAnimation ? 'hero-animate' : ''}`}>
             <div className='hero-content'>
-                {shuffledPhotos.slice(0, 3).map((photo, index) => (
-                    <div
-                        key={index}
-                        className='photo-div'
-                        style={{
-                            backgroundImage: `url(${photo})`,
-                            top: index === 0 ? `${getRandomNumber(0, 60)}%` : index === 1 ? `${getRandomNumber(0, 10)}%` : 'auto',
-                            left: index === 0 ? `${getRandomNumber(0, 32)}%` : 'auto',
-                            right: index === 1 || index === 2 ? `${getRandomNumber(0, 32)}%` : 'auto',
-                            bottom: index === 2 ? `${getRandomNumber(0, 10)}%` : 'auto',
-                        }}
-                    />
-                ))}
+                {shuffledPhotos.slice(0, 3).map((photo, index) => {
+                    const maxWidthPercentage = index === 0 ? 30 : 25; 
+                    let widthPercentage;
+                    if (index === 0) {
+                        widthPercentage = getRandomNumber(30, maxWidthPercentage) * 1.25;
+                    } else {
+                        widthPercentage = getRandomNumber(25, maxWidthPercentage);
+                    }
+                    return (
+                        <div
+                            key={index}
+                            className='photo-div'
+                            style={{
+                                backgroundImage: `url(${photo})`,
+                                top: index === 0 ? `${getRandomNumber(0, 30)}%` : index === 1 ? `${getRandomNumber(0, 10)}%` : 'auto',
+                                left: index === 0 ? `${getRandomNumber(0, 10)}%` : 'auto',
+                                right: index === 1 || index === 2 ? `${getRandomNumber(0, 20)}%` : 'auto',
+                                bottom: index === 2 ? `${getRandomNumber(0, 10)}%` : 'auto',
+                                width: `${widthPercentage}%`,
+                                // maxWidth: `${contentWidth}%`, 
+                            }}
+                        />
+                    );
+                })}
             </div>
         </section>
     );
