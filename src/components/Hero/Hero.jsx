58e121ef3c78d8fe61d7f0ba1d886987be0e4f8f
Melanie Winter <melaniebucher830@gmail.com>
2024-05-10 22:40:04 -0700

    Adds logic for random placement of images on hero component

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,35 +1,62 @@
-import './Hero.css'
+import './Hero.css';
 
 export default function Hero({ playAnimation }) {
 
     function getAllPhotos() {
-        const basePath = '../assets/Images/spacelab-pics/'
-        const numPhotos = 4
+        const basePath = './assets/Images/';
+        const numPhotos = 4;
     
-        const photos = []
+        const photos = [];
     
         for (let i = 1; i <= numPhotos; i++) {
-            const photoPath = `${basePath}photo-${i}.jpeg`
-            photos.push(photoPath)
+            const photoPath = `${basePath}photo-${i}.jpeg`;
+            photos.push(photoPath);
         }
     
-        return photos
+        return photos;
     }
     
     const allPhotos = getAllPhotos();
-    console.log(allPhotos)
+
+    function getRandomNumber(min, max) {
+        return Math.floor(Math.random() * (max - min + 1) + min);
+    }
 
     return (
         <section className={`Hero ${playAnimation ? 'hero-animate' : ''}`}>
             <div className='hero-content'>
-            <div
-                className='photo-div'
-                style={{
-                    backgroundImage: `url(${allPhotos[0]})`,
-                }}
-            >
-            </div>
+                {allPhotos.map((photo, index) => (
+                    <>
+                        <div
+                            key={index}
+                            className='photo-div'
+                            style={{
+                                backgroundImage: `url(${photo})`,
+                                top: `${getRandomNumber(0, 60)}%`,
+                                left: `${getRandomNumber(0, 30)}%`,
+                            }}
+                        />
+                        <div
+                            key={index}
+                            className='photo-div'
+                            style={{
+                                backgroundImage: `url(${photo})`,
+                                top: `${getRandomNumber(0, 10)}%`,
+                                right: `${getRandomNumber(0, 30)}%`,
+                            }}
+                        />
+                                                <div
+                            key={index}
+                            className='photo-div'
+                            style={{
+                                backgroundImage: `url(${photo})`,
+                                bottom: `${getRandomNumber(0, 10)}%`,
+                                right: `${getRandomNumber(0, 30)}%`,
+                            }}
+                        />
+                    </>
+                ))}
             </div>
         </section>
-    )
+    );
 }
