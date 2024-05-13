e013f612f5892d6d388b09b46de82f877647eb00
Melanie Winter <melaniebucher830@gmail.com>
2024-05-12 22:06:35 -0700

    Adds photo integration and copy content to hero content container

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,56 +1,72 @@
-import './Hero.css';
+import { useEffect, useState } from 'react'
+import Logo from "../../../public/assets/spacelab.svg"
+import './Hero.css'
 
 export default function Hero({ playAnimation }) {
+    const [activePhotos, setActivePhotos] = useState([])
+    const [showPhotos, setShowPhotos] = useState([false, false, false]);
+
     function getAllPhotos() {
-        const basePath = './assets/Images/';
-        const numPhotos = 4;
+        const basePath = './assets/Images/'
+        const numPhotos = 4
     
-        const photos = [];
+        const photos = []
     
         for (let i = 1; i <= numPhotos; i++) {
-            const photoPath = `${basePath}photo-${i}.jpeg`;
-            photos.push(photoPath);
+            const photoPath = `${basePath}photo-${i}.jpeg`
+            photos.push(photoPath)
         }
     
-        return photos;
+        return photos
     }
     
-    const allPhotos = getAllPhotos();
-
-    function getRandomNumber(min, max) {
-        return Math.floor(Math.random() * (max - min + 1) + min);
-    }
+    const allPhotos = getAllPhotos()
 
     const shuffledPhotos = allPhotos.sort(() => Math.random() - 0.5)
+
+    useEffect(() => {
+        const photoSet = shuffledPhotos.slice(0, 3)
+        setActivePhotos(photoSet)
+    }, [])
+
+    useEffect(() => {
+        activePhotos.forEach((_, index) => {
+            setTimeout(() => {
+                setShowPhotos(prevShowPhotos => {
+                    const updatedShowPhotos = [...prevShowPhotos]
+                    updatedShowPhotos[index] = true
+                    return updatedShowPhotos
+                })
+            }, index * 3000)
+        })
+    }, [activePhotos])
 
     return (
         <section className={`Hero ${playAnimation ? 'hero-animate' : ''}`}>
             <div className='hero-content'>
-                {shuffledPhotos.slice(0, 3).map((photo, index) => {
-                    const maxWidthPercentage = 30; 
-                    let widthPercentage;
-                    if (index === 0) {
-                        widthPercentage = getRandomNumber(28, maxWidthPercentage) * 1.25;
-                    } else {
-                        widthPercentage = getRandomNumber(15, maxWidthPercentage);
-                    }
+                {activePhotos.map((photo, index) => {
                     return (
-                        <div
+                        <div 
                             key={index}
-                            className='photo-div'
+                            className='photo'
                             style={{
                                 backgroundImage: `url(${photo})`,
-                                top: index === 0 ? '1rem' : index === 1 ? `${getRandomNumber(0, 8)}%` : 'auto',
-                                left: index === 0 ? `${getRandomNumber(0, 8)}%` : 'auto',
-                                right: index === 1 ? `${getRandomNumber(15, 20)}%` : index === 2 ? `${getRandomNumber(0, 5)}%` : 'auto',
-                                bottom: index === 2 ? `${getRandomNumber(0, 8)}%` : 'auto',
-                                width: `${widthPercentage}%`,
-                                // maxWidth: `${contentWidth}%`, 
+                                backgroundSize: `${showPhotos[index] ? '100' : '0'}%`,
+                                opacity: `${showPhotos[index] ? '30' : '0'}%`,
                             }}
-                        />
-                    );
+                        >
+                        </div>
+                    )
                 })}
+                <div className='look-up'>
+                    Who do you see when you look up?...
+                </div>
+                <div className='hero-mission'>
+                    <span>OUR MISSION:</span>
+                    <span> More jobs for People of Color, Women and the LGBTQ+ Communities in Astronomy, Data Science and Technology. Joi us by Donating!</span>
+                </div>
+                <img src={Logo} alt="logo" className='hero-logo'/>
             </div>
         </section>
-    );
+    )
 }
